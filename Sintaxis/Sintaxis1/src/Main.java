7ff082ad01c5b8a1777acc07807ee8693ba94eac
dani-od02 <andradebotello930@gmail.com>
2025-01-23 14:59:49 -0600

    Estructura de Java, ejemplo de cadena

---
+++ Sintaxis/Sintaxis1/src/Main.java
@@ -0,0 +1,27 @@
+//Zona de importaciones
+
+
+//Clase principal
+public class Main {
+    //metodo Main/metodo principal
+    public static void main(String[] args) {
+
+        /* Comentario
+            Varias
+            Lineas
+         */
+
+
+
+ // 2. Cadenas
+        // ejemplo concatenar cadena
+        String cadena= "Dani " + " Andrade " + " Botello ";
+        System.out.println(cadena);
+        //dar longitud de la cadena
+        System.out.println(cadena.length());
+        //extracion de caracteres en base a parametros
+        System.out.println(cadena.substring(2,5));
+        System.out.println(cadena.substring(2));
+        System.out.println(cadena.substring(0,5));
+    }
+}

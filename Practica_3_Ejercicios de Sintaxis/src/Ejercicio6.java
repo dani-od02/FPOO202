f96a48db10bdf5902613e6f5b75ed8bedea11043
dani-od02 <andradebotello930@gmail.com>
2025-01-28 15:54:14 -0600

    Ejercicio 6
    6. Crea un programa que pida al usuario que introduzca una frase  y muestre
    en pantalla la frase invertida.

--- Practica_3_Ejercicios de Sintaxis/src/Ejercicio6.java
+++ Practica_3_Ejercicios de Sintaxis/src/Ejercicio6.java
@@ -1,2 +1,17 @@
+
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Ingrese una frase: ");
+        String frase = scn.nextLine();
+
+        String fraseinvertida = new StringBuilder(frase).reverse().toString();
+
+        System.out.println("Frase invertida: " + fraseinvertida);
+
+        scn.close();
+    }
 }

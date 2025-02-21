a583b2df7f1e923f5f25baeecb3339942ef0408c
dani-od02 <andradebotello930@gmail.com>
2025-02-21 14:11:42 -0600

    practica 9
    creacion de codigo con JOption

---
+++ Practica9/src/Cuenta.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class Cuenta {
+    int noCuenta;
+    String titular;
+    float saldo;
+
+    public float ConsultarSaldo() {
+    JOptionPane.showMessageDialog(null, "Saldo: " + saldo);
+    return saldo;
+    }
+    public float IngresarEfectivo(float monto) {
+    saldo += monto;
+    JOptionPane.showMessageDialog(null, "Ingresado: " + monto + "\nSaldo: " + saldo);
+    return saldo;
+    }
+    public float RetirarEfectivo(float monto) {
+        if (monto > saldo) {
+        JOptionPane.showMessageDialog(null, "Saldo insuficiente");
+        } else {
+        saldo -= monto;
+        JOptionPane.showMessageDialog(null, "Retirado: " + monto + "\nSaldo: " + saldo);
+        }
+        return saldo;
+    }
+
+
+}
---
+++ Practica9/src/Main.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        Cuenta cuenta = new Cuenta();
+        cuenta.noCuenta = 122046026;
+
+        cuenta.titular = JOptionPane.showInputDialog("Ingrese su nombre :");
+        cuenta.saldo = Float.parseFloat(JOptionPane.showInputDialog("Ingrese su saldo inicial :"));
+
+        String opcion;
+        do {
+            opcion = JOptionPane.showInputDialog(
+                    "Eliga una opción:\n" +
+                            "1. Consultar saldo\n" +
+                            "2. Ingresar efectivo\n" +
+                            "3. Retirar efectivo\n" +
+                            "4. Salir"
+            );
+
+            if (opcion == null) break;
+
+            switch (opcion) {
+                case "1":
+                    cuenta.ConsultarSaldo();
+                    break;
+                case "2":
+                    float ingreso = Float.parseFloat(JOptionPane.showInputDialog("Monto a ingresar:"));
+                    cuenta.IngresarEfectivo(ingreso);
+                    break;
+                case "3":
+                    float retiro = Float.parseFloat(JOptionPane.showInputDialog("Monto a retirar:"));
+                    cuenta.RetirarEfectivo(retiro);
+                    break;
+                case "4":
+                    JOptionPane.showMessageDialog(null, "Vuelva pronto!");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+        } while (!"4".equals(opcion));
+    }
+}
